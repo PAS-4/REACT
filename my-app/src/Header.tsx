bfa671c1458c1963dd6ed62fd5e4d00477cba37d
Patrese Ávila <64747567+PAS-4@users.noreply.github.com>
2021-09-01 00:17:14 -0300

    first commit

---
+++ my-app/src/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function header(){
+    return(
+        <div className="header">
+            <div className="container">
+                <h1>LOGOTIPO</h1>
+                <ul>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default header;
---
+++ my-app/src/Slide.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function slide(){
+    return(
+        <div className="slide">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box"></div>
+        </div>
+    )
+}
+
+export default slide;
